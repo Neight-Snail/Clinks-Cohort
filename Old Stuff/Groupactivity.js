93cefa321a55b26c8e8c2ba3ecbe5f70311f4472
Nate <natestarlin7@gmail.com>
2022-04-30 14:48:32 -0600

    Firt Commit

---
+++ Old Stuff/Groupactivity.js
@@ -0,0 +1,6 @@
+function CountPositives(arrayinput){
+    var posCount=0
+    var negCount=0
+    for(var i-0; i<arrayinput.length; i++)
+        if(arrayInput[i]>0)
+}
---
+++ Old Stuff/algo.js
@@ -0,0 +1,13 @@
+var newarray = [4, true, "no", null]
+
+console.log(newarray[2])
+
+if(newarray[0]<2){
+    console.log("Bigger than 2")
+}
+else if(newarray[1]==true){
+    console.log("True is the answer")
+}
+else{
+    console.log("neither was true")
+}
